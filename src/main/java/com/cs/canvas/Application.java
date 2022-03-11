87e1c08038763950c2dd2841d38a3852a317d4ab
Sarawanan <sarawanan.mahalingam@icloud.com>
2022-03-11 08:52:59 +0530

    Add try catch to application

--- src/main/java/com/cs/canvas/Application.java
+++ src/main/java/com/cs/canvas/Application.java
@@ -6,10 +6,14 @@ public class Application {
     public static void main(String[] args) throws InvalidCommandException {
         try (Scanner scanner = new Scanner(System.in)) {
             do {
-                System.out.print("Enter Command : ");
-                String input = scanner.nextLine();
-                Shape shape = new ShapeFactory().getShape(Utils.extractCommand(input));
-                System.out.println(shape.draw(Utils.extractParams(input)));
+                try {
+                    System.out.print("Enter Command : ");
+                    String input = scanner.nextLine();
+                    Shape shape = new ShapeFactory().getShape(Utils.extractCommand(input));
+                    System.out.println(shape.draw(Utils.extractParams(input)));
+                } catch (InvalidCommandException e) {
+                    e.printStackTrace();
+                }
             } while (true);
         }
     }
